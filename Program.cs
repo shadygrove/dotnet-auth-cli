52209aebd36591e7f4e10d87be6864fb20881acb
Martin Bour <mbour@topcon.com>
2021-08-20 11:19:32 -0400

    Init Weather CLI

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_weather_cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
